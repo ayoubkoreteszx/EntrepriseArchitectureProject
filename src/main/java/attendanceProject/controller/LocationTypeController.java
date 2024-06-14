230e9ff915b540e5b39ffa1c5481076bee3e89dd
mkoner <amzid@hotmail.fr>
2024-06-14 00:04:20 -0500

    Use-case12: Added controller for location type

---
+++ src/main/java/attendanceProject/controller/LocationTypeController.java
@@ -0,0 +1,56 @@
+package attendanceProject.controller;
+
+import attendanceProject.domain.LocationType;
+import attendanceProject.repository.LocationRepository;
+import attendanceProject.service.locationTypeService.LocationTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/sys-admin/location-types")
+public class LocationTypeController {
+    @Autowired
+    private LocationTypeService locationTypeService;
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        return new ResponseEntity<List<LocationType>>(
+                locationTypeService.findAllLocationTypes(),
+                HttpStatus.OK
+        );
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        LocationType locationType = locationTypeService.findLocationTypeById(id);
+        if (Objects.isNull(locationType)) {
+            return new ResponseEntity<LocationType>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<LocationType>(locationType, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<?> createLocationType(@RequestBody LocationType locationType) {
+        return new ResponseEntity<LocationType>(
+                locationTypeService.createLocationType(locationType),
+                HttpStatus.CREATED
+        );
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateLocationType(@PathVariable Long id, @RequestBody LocationType locationType) {
+        return new ResponseEntity<LocationType>(
+                locationTypeService.updateLocationType(id, locationType),
+                HttpStatus.OK
+        );
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteLocationType(@PathVariable Long id) {
+        locationTypeService.deleteLocationType(id);
+        return new ResponseEntity<LocationType>(HttpStatus.NO_CONTENT);
+    }
+}
