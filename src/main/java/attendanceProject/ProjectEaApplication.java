8838b2d531cc04118375134fee0e29aea7ea7a1f
Abdallah Elsharawy <abdallahgaber15@gmail.com>
2024-06-15 18:26:08 -0500

    Use Cases: (3) Done but not totally tested. need further classes. (11) Done and tested on Postman.

--- src/main/java/attendanceProject/ProjectEaApplication.java
+++ src/main/java/attendanceProject/ProjectEaApplication.java
@@ -30,59 +30,6 @@ public class ProjectEaApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-/*
-        Student student = new Student();
-        student.setStudentId("TEST_St_ID");
-
-        LocationType locationType = new LocationType();
-        locationType.setType("LocationTypeTest");
-        Location location = new Location();
-        location.setLocationType(locationType);
-        location.setName("LocationNameTest");
-        Session session = new Session();
-        session.setDate(LocalDate.now());
-        session.setStartTime(LocalTime.MIDNIGHT);
-        session.setEndTime(LocalTime.NOON);
-        attendanceRecordService.saveAttendanceRecord(new AttendanceRecord(
-                LocalDateTime.now(),
-                student,
-                location,
-                session
-        ));
-        */
-
-
-
-/*
-        Course course = new Course();
-        course.setCourseCode("CS544");
-        course.setAudit(new Audit(
-                LocalDate.of(2024,6,14),
-                LocalDate.of(2024,6,15),
-                "Abdallah", "Gaber"));
-        course.setCourseName("Enterprise Architecture");
-        course.setCourseDescription("Spring Course");
-        course.setCredits(500);
-        course.setDepartment("ComPro");
-        course.setPrerequisites(null);
-
-        courseService.saveCourse(course);
-
-        Course course2 = new Course();
-        course2.setCourseCode("CS500");
-        course2.setAudit(new Audit(
-                LocalDate.of(2024,6,14),
-                LocalDate.of(2024,6,15),
-                "PAbdallah", "PGaber"));
-        course2.setCourseName("PEnterprise Architecture");
-        course2.setCourseDescription("PSpring Course");
-        course2.setCredits(400);
-        course2.setDepartment("PComPro");
-        course2.setPrerequisites(Arrays.asList(course));
-
-        courseService.saveCourse(course2);
-*/
-
 
 
 
