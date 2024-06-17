f2ffe5eb514561fc32ccc972f31464afd436a23b
Darkhan Zhursin <daratechn@gmail.com>
2024-06-17 18:44:27 -0500

    course offering dto request

--- src/main/java/attendanceProject/controller/webClientConfig/ResponseMessage.java
+++ src/main/java/attendanceProject/controller/webClientConfig/ResponseMessage.java
@@ -1,10 +1,13 @@
 package attendanceProject.controller.webClientConfig;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 @Schema(description = "Response message")
 public class ResponseMessage {
     private String message;
+    public ResponseMessage() {}
 }
