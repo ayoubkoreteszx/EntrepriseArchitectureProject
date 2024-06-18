05f4dc5ecdbb14c990ada67014852095ce5a59b2
mkoner <amzid@hotmail.fr>
2024-06-18 18:58:37 -0500

    Created Admin view and student view service, moved use-case n5 to admin view service

--- src/main/java/attendanceProject/controller/AttendanceRecordController.java
+++ src/main/java/attendanceProject/controller/AttendanceRecordController.java
@@ -2,14 +2,9 @@ package attendanceProject.controller;
 
 import attendanceProject.controller.Dto.attendance.AttendanceRecordDTORequest;
 import attendanceProject.controller.Dto.attendance.AttendanceRecordDTOResponse;
-import attendanceProject.domain.AttendanceRecord;
 import attendanceProject.service.attendanceRecordService.AttendanceRecordService;
-import attendanceProject.service.attendanceRecordService.ExcelExportService;
-import attendanceProject.service.courseOfferingService.CourseOfferingService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import attendanceProject.controller.Dto.attendance.AttendanceRecordMapper;
@@ -17,10 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/attendance-records")
