d0ad054ba5d99d348901d4427031a6e057cd3dd5
mkoner <amzid@hotmail.fr>
2024-06-18 21:21:01 -0500

    Moved use-case2 to student view service

---
+++ StudentView/src/main/java/eaproject/studentview/feignClients/AttendanceSystemClient.java
@@ -0,0 +1,15 @@
+package eaproject.studentview.feignClients;
+
+import eaproject.studentview.controller.dto.AttendanceRecordDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "AttendanceSystem", url = "http://localhost:8080")
+public interface AttendanceSystemClient{
+    @GetMapping("/attendance-records/student/{studentId}/course-offering/{offeringId}")
+    List<AttendanceRecordDTOResponse> getStudentAttendanceRecordsForCourseOffering(
+            @PathVariable long studentId, @PathVariable long offeringId);
+}
